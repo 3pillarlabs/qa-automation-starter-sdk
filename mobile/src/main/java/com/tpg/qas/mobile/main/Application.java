48a26ff55d2dba5f9aea83ca6f2042f77d53cd5e
Amit Bharti <amitb.mca@gmail.com>
2018-02-15 21:12:48 +0530

    Adding Framework Directory Structure With Gradle Build

---
+++ mobile/src/main/java/com/tpg/qas/mobile/main/Application.java
@@ -0,0 +1,12 @@
+package com.tpg.qas.mobile.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
