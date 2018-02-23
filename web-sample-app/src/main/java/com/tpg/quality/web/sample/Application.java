0c5fefb70e0d2337736e21ce2d43d4883c0e9f38
Amit Bharti <amitb.mca@gmail.com>
2018-02-23 15:24:07 +0530

    Modified file to enable Spring Boot

--- web-sample-app/src/main/java/com/tpg/quality/web/sample/Application.java
+++ web-sample-app/src/main/java/com/tpg/quality/web/sample/Application.java
@@ -1,5 +1,8 @@
 package com.tpg.quality.web.sample;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
